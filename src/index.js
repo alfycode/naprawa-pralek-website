fac0fab57b034af88f2d7ac8286c60e3215e4858
Kamil <kamilszalast0@gmail.com>
2022-03-29 20:34:52 +0200

    ALFY-32: feat: add MessengerButton component version 1.0, set up MessengerChat cmponent inside with proper color and language

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import MessengerButton from "./MessengerButton";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <MessengerButton />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
