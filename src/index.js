0735f9806cee57aba82a00d243aea9941fe0255c
Kamil <kamilszalast0@gmail.com>
2022-04-02 19:01:58 +0200

    fix: changed MessengerButton from npm dependency to directly pasted code to index.html from Facebook Website

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import MessengerButton from "./MessengerButton";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MessengerButton />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
