062dfc2f50cb757274e23a32d43916a79c778ee9
Kamil <kamilszalast0@gmail.com>
2022-04-07 19:45:28 +0200

    fix: edit content of  about us page

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -12,6 +12,9 @@ import {
   ClientNameText,
   ClientOpinionTextWrap,
   ClientOpinionText,
+  TextParagraph,
+  OpinionsLink,
+  GoogleWebsiteButton,
 } from "./AboutUsElements";
 import { Bubble } from "../Landing/LandingElements";
 import { theme } from "../../helpers/theme";
@@ -27,11 +30,27 @@ const AboutUsPage = () => {
           whileInView={{ opacity: 1, transition: { duration: 1, delay: 0.2 } }}
           viewport={{ once: true }}
         >
-          Zajmuję się naprawą pralek wszystkich typów już od 25 lat. 23-letnie
-          doświadczenie, najwyższa jakość usług w zakresie napraw pralek.
-          Naprawy wykonuję na terenie Lublina i dalszych okolic, w domu klienta.
-          Naprawiam moduły elektroniczne sterujące pralką (sterowniki). Posiadam
-          szerokie grono zaufanych klientów.
+          <TextParagraph>
+            Zajmuję się naprawą pralek wszystkich firm oraz wszystkich typów od
+            ponad 30 lat. Zdobyte doświadczenie przekłada się na szybką diagnozę
+            usterki oraz sprawną realizację naprawy Państwa sprzętu AGD.
+          </TextParagraph>
+          <TextParagraph>
+            O przewidywanym koszcie naprawy klient zazwyczaj dowiaduje się już
+            na etapie rozmowy telefonicznej. Swoje usługi serwisowe wykonuję
+            wykonuję bezpośrednio w domu klienta, więc nie muszą się Państwo
+            marwić o transport pralki. Naprawy wykonuję na terenie Lublina, a
+            nawet całego województwa lubelskiego.
+          </TextParagraph>
+          <TextParagraph>
+            Naprawiam także moduły elektroniczne sterujące pralką (sterowniki),
+            co pozwala na obniżenie kosztów naprawy, względem kosztów wymiany
+            całego układu elektronicznego. Posiadam szerokie grono zaufanych
+            klientów, którzy skorzystali już w moich usług serwisowych.
+          </TextParagraph>
+          <OpinionsLink href={"https://g.page/r/CRNUyEpn1OkuEAE"}>
+            <GoogleWebsiteButton>Profil firmy w Google</GoogleWebsiteButton>
+          </OpinionsLink>
         </AboutUsText>
       </AboutUsContentWrap>
       <AboutUsContentWrap second right='true'>
