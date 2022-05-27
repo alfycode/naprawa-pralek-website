d52306dd6fd87127b4700bea04d4cecbe2f11edf
dekzzie <perecadrian@gmail.com>
2022-05-27 19:54:02 +0200

    main : fix container padding

--- src/components/Container.js
+++ src/components/Container.js
@@ -9,7 +9,7 @@ export const Container = styled.div`
   width: 100%;
   background: ${(props) =>
     props.bgColor ? theme.colors.whiteLinear : theme.colors.darkBlue};
-  padding: ${(props) => (props.landing ? "80px" : "0")} 2rem 0 2rem;
+  padding: ${(props) => (props.landing ? "80px" : "0")} 1rem 0 1rem;
   display: flex;
   flex-direction: column;
   align-items: center;
